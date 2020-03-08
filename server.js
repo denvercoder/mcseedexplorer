ec58268c3ae3031f1cd674a2aab57fc939bdb075
Tim Myers <tim@denvercoder.com>
2020-03-08 15:38:00 -0600

    maybe...maybe...maybe...

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use(cors())
 app.use(express.json())
@@ -32,15 +33,14 @@ const usersRouter = require('./routes/users')
 app.use('/seeds', seedsRouter)
 app.use('/users', usersRouter)
 
-// Serve Static Assets
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')))
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
   })
 }
 
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
 })
