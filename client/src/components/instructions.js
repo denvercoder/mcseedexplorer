c3c798c77df3331da066330b1a5b3a77bc18a725
Tim Myers <tim@denvercoder.com>
2020-03-08 16:29:54 -0600

    added instructions

--- client/src/components/instructions.js
+++ client/src/components/instructions.js
@@ -4,7 +4,23 @@ export default class Instructions extends Component {
   render() {
     return (
       <div>
-        <p>Instructions</p>
+        <h3>Instructions</h3>
+        <p>
+          <a href='https://www.youtube.com/watch?v=MCRYNyW2jtU'>
+            Here is a quick video I made showing how to use this current
+            version.
+          </a>
+        </p>
+        <p>
+          HERE IS HOW TO USE IT https://mcseedexplorer.com Go to the URL above.
+          Click "Create User" and enter a username. It must be more than 5
+          characters. Enter your username and REMEMBER IT When you add a seed
+          you will need to enter in the username. After you've clicked the
+          "Create New User" button it will take you to the main page. Then click
+          "Create Seed" Enter all of your information and then click "Add Seed"
+          You will then be taken back to the main page and you'll be able to see
+          your seed you just added, (hopefully)
+        </p>
       </div>
     )
   }
