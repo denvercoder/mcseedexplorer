02e27bdaacf5370237cd9833fe6ebecbd1479392
Tim Myers <tim@denvercoder.com>
2020-03-08 15:46:53 -0600

    YAY

--- client/src/components/create-seed.js
+++ client/src/components/create-seed.js
@@ -62,9 +62,7 @@ export default class CreateSeed extends Component {
       hasCleric: this.state.hasCleric
     }
 
-    axios
-      .post('http://localhost:5000/seeds/add', seed)
-      .then(res => console.log(res.data))
+    axios.post('/seeds/add', seed).then(res => console.log(res.data))
 
     window.location = '/'
   }
