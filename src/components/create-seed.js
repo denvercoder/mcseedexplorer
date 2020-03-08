58abe7483e6d7a0011ff382305de5ef4d2eb2f82
Tim Myers <tim@denvercoder.com>
2020-03-08 15:19:17 -0600

    trying to get this working 34

--- src/components/create-seed.js
+++ src/components/create-seed.js
@@ -62,7 +62,9 @@ export default class CreateSeed extends Component {
       hasCleric: this.state.hasCleric
     }
 
-    axios.post('/seeds/add', seed).then(res => console.log(res.data))
+    axios
+      .post('http://localhost:5000/seeds/add', seed)
+      .then(res => console.log(res.data))
 
     window.location = '/'
   }
