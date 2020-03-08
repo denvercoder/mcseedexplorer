bc5ad4de01a0ac4557d19a124818843dd7beedc3
Tim Myers <tim@denvercoder.com>
2020-03-08 09:03:39 -0600

    add env vars

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+const publicPath = path.join(__dirname, '..', 'public')
+
 const mongoose = require('mongoose')
 
 require('dotenv').config()
@@ -11,6 +13,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(publicPath))
 
 const uri = process.env.ATLAS_URI
 
